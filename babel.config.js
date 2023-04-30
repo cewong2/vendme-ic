474b2a687ebac2a53b71a6ee9024c9ba3fbb054b
C. Wong <cewong2@thewon.gs>
2023-04-30 17:22:30 +0900

    webpack errors

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,14 @@
 module.exports = {
   presets: [
-    '@vue/cli-plugin-babel/preset',
-    ['@babel/preset-env', { useBuiltIns: 'entry', corejs: 3 }]
+    [
+      '@babel/preset-env',
+      {
+        useBuiltIns: 'usage',
+        corejs: 3
+      }
+    ]
+  ],
+  plugins: [
+    '@babel/plugin-transform-runtime'
   ]
 }
--- vue.config.js
+++ vue.config.js
@@ -1,20 +1,15 @@
-const webpack = require('webpack');
-
 module.exports = {
-  configureWebpack: {
-    plugins: [
-      new webpack.DefinePlugin({
-        'process.env': {
-          NODE_ENV: JSON.stringify(process.env.NODE_ENV),
-          VUE_APP_API_BASE_URL: JSON.stringify(process.env.VUE_APP_API_BASE_URL)
-        }
-      })
-    ]
-  },
   chainWebpack: config => {
-    config.plugin('html').tap(args => {
-      args[0].title = 'VendMe Image Capture';
-      return args;
-    });
+    // Set whitespace property
+    config.module.rule('vue').use('vue-loader').tap(options => {
+      options.compilerOptions.whitespace = 'preserve'
+      return options
+    })
+  },
+  pluginOptions: {
+    'style-resources-loader': {
+      preProcessor: 'scss',
+      patterns: []
+    }
   }
-};
+}
