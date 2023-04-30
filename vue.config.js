d8975b05f941bb33c0a16d662bd4ca72cdee3ebe
C. Wong <cewong2@thewon.gs>
2023-04-30 17:57:32 +0900

    3

--- vue.config.js
+++ vue.config.js
@@ -1,14 +1,58 @@
+const webpack = require('webpack');
+const path = require('path');
+const defaultWebpackConfig = require('./webpack.config.js');
+//const CompressionPlugin = require('compression-webpack-plugin');
+
 module.exports = {
+  devServer: {
+    disableHostCheck: true
+  },
+  configureWebpack: {
+    plugins: [
+      new webpack.DefinePlugin({
+        'process.env': {
+          NODE_ENV: JSON.stringify(process.env.NODE_ENV)
+        }
+      })
+    ]
+  },
   chainWebpack: config => {
+    if (defaultWebpackConfig.module) {
+      const urlLoader = defaultWebpackConfig.module.rule('url-loader');
+      urlLoader
+        .test(/\.(png|jpg|gif|svg|woff|woff2|ttf|eot)$/)
+        .use('url-loader')
+        .loader('url-loader')
+        .tap(options => Object.assign(options, { limit: 8192, name: '[name].[ext]?[hash]' }));
+    }
+
     config.module
-      .rule("vue")
-      .use("vue-loader")
-      .tap(options =>
-        Object.assign(options, {
-          compilerOptions: {
-            whitespace: "condense"
-          }
-        })
-      );
+      .rule('babel')
+      .use('babel-loader')
+      .tap(options => {
+        options.presets = ['@babel/preset-env'];
+        options.plugins = ['@babel/plugin-transform-runtime'];
+        return options;
+      });
+    
+    config.plugin('compression-webpack-plugin').use(CompressionPlugin);
+
+    config.module
+      .rule('vue')
+      .use('vue-loader')
+      .tap(options => {
+        options.transformAssetUrls = {
+          img: 'src',
+          'b-avatar': 'src',
+          'b-img': 'src',
+          'b-img-lazy': ['src', 'blank-src'],
+          'b-card': 'img-src',
+          'b-card-img': 'src',
+          'b-card-img-lazy': ['src', 'blank-src'],
+          'b-carousel-slide': 'img-src',
+          'b-embed': 'src'
+        };
+        return options;
+      });
   }
-}
+};
