39a378487d3f8c066f38a5f5f79f5eeec70f0a13
C. Wong <cewong2@thewon.gs>
2023-04-30 17:29:05 +0900

    2

--- vue.config.js
+++ vue.config.js
@@ -1,15 +1,14 @@
 module.exports = {
   chainWebpack: config => {
-    // Set whitespace property
-    config.module.rule('vue').use('vue-loader').tap(options => {
-      options.compilerOptions.whitespace = 'preserve'
-      return options
-    })
-  },
-  pluginOptions: {
-    'style-resources-loader': {
-      preProcessor: 'scss',
-      patterns: []
-    }
+    config.module
+      .rule("vue")
+      .use("vue-loader")
+      .tap(options =>
+        Object.assign(options, {
+          compilerOptions: {
+            whitespace: "condense"
+          }
+        })
+      );
   }
 }
