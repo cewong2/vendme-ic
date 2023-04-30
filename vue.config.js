ea52ca8d76dbe78179b53a396b6f508949688e34
C. Wong <cewong2@thewon.gs>
2023-04-30 17:06:47 +0900

    updated vue.config

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,28 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+module.exports = {
+  configureWebpack: {
+    module: {
+      rules: [
+        {
+          test: /\.mjs$/,
+          include: /node_modules/,
+          type: "javascript/auto",
+        },
+        {
+          test: /\.worker\.js$/,
+          use: { loader: "worker-loader" },
+        },
+      ],
+    },
+  },
+  chainWebpack: (config) => {
+    config.module
+      .rule("vue")
+      .use("vue-loader")
+      .tap((options) => {
+        options.compilerOptions.whitespace = "preserve";
+        return options;
+      });
+  },
+  transpileDependencies: ["vuetify"],
+  publicPath: process.env.NODE_ENV === "production" ? "/vendme-ic/" : "/",
+};
