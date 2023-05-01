b07cd7b19c72b33732107ef9e05661d49d7f6d1c
C. Wong <cewong2@thewon.gs>
2023-05-01 16:33:24 +0900

    Babel Update

---
+++ webpack.js
@@ -0,0 +1,11 @@
+module: {
+    rules: [
+      {
+        test: /(\.js[\S]{0,1})$/i,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['@babel/preset-react', '@babel/preset-env'],
+          plugins: ['@babel/proposal-class-properties']
+        },
+      },
